32e23923bfa019e07fa5153bf26b0c70e488a125
Basiliotornado <41091910+Basiliotornado@users.noreply.github.com>
2024-04-01 22:50:57 -0400

    Update crush.py

--- crush.py
+++ crush.py
@@ -4,10 +4,14 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument('name')
-parser.add_argument('-i', '--iterations', type=int)
+parser.add_argument('-i', '--iterations', type=int,default=8)
 parser.add_argument('-b', '--bitrate')
+parser.add_argument('-crf', '--crf')
+parser.add_argument('-l', '--length', type=int, default=10)
 parser.add_argument('-s', '--scale')
+parser.add_argument('-ts', '--tmixscale', type=float, default=1)
 parser.add_argument('-o', '--output')
+parser.add_argument('-m', '--mode', type=int, default=0)
 
 args = parser.parse_args()
 
@@ -24,13 +28,29 @@ def iter(name_in, name_out):
     if args.bitrate:
         command += f"-b:v {args.bitrate} "
 
+    if args.crf:
+        command += f"-crf {args.crf} "
+
     command += f"-vf \""
 
     if args.scale:
         command += f"scale=\"{args.scale}\","
 
     #command += "format=gbrp,tmix=frames=19:weights='-1 -2 -3 -4 -5 -6 -7 -8 -9 91 -9 -8 -7 -6 -5 -4 -3 -2 -1'[v];[v]format=yuv420p\" " 
-    command += "format=gbrp,tmix=frames=10:weights='-1 -2 -3 -4 -5 -6 -7 -8 -9 46'[v];[v]format=yuv420p\" " 
+    command += "format=gbrp,tmix"
+
+    match args.mode:
+        case 0:
+            command += f"=frames={args.length}:weights='"
+            nums = range(1, args.length)
+            for i in nums:
+                command += f"-{i} "
+
+            command += f"{sum(nums) + 1}':scale={1/args.length if args.tmixscale == 0.0 else args.tmixscale}"
+        case 1:
+            command += "=frames=2:weights='-1 2':scale=1"
+
+    command += "[v];[v]format=yuv420p\" "
 
     command += f"{folder}/{name_out}"
 
